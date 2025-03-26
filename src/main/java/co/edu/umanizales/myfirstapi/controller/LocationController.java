62a4a9367bc69f4daebf976a3dd75b7ceb5b4db6
santa <lsantana113786@umanizales.edu.co>
2025-03-26 17:43:15 -0500

    Mi primera version

--- src/main/java/co/edu/umanizales/myfirstapi/controller/LocationController.java
+++ src/main/java/co/edu/umanizales/myfirstapi/controller/LocationController.java
@@ -15,8 +15,10 @@ public class LocationController {
     public String Getlocation() {
 
         Seller juan = new Seller("12345678", "juan", "ospina", 'M', (byte)23, new Location("05001", "medellin"));
-        Seller alex = new Seller("09876453", "alex", "navarro", 'M', (byte)29, new Location("05002", "medellin"));
-        Seller daniela = new Seller("49831276", "daniela", "gallego", 'F', (byte)56, new Location("05003", "medellin"));
+        Seller alex = new Seller("09876453", "alex", "navarro", 'M', (byte)29, new Location("05044","anzá"));
+        Seller daniela = new Seller("49831276", "daniela", "gallego", 'F', (byte)56, new Location("05021", "alejandria"));
+        Seller alejandra = new Seller("43129034", "alejandra", "venegas", 'F', (byte)18, new Location("05004", "abriaqí"));
+        Seller victor = new Seller("98765432", "victor", "silva", 'M', (byte) 35, new Location("05031", "amalfi"))
         return "ukraina";
 
     }
